c4a669e6a0804b4717041e0a3f66392c258dafbc
Archibaldo De La Cruz <vimerinom@gmail.com>
2018-01-21 01:55:36 -0300

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,10 +1,13 @@
 /**
+* Domain classes used to produce .....
+* <p>
+* These classes contain the ......
+* </p>
 *
 * @since 1.0
 * @author somebody
 * @version 1.0
 */
-
 package cl.victormerino;
 
 /**
